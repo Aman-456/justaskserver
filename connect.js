a7f22da8cb60fb33b1048b3dbd4c5d7268331fc1
rapidev-1 <rapidev.softwares@gmail.com>
2022-10-07 00:34:19 +0500

    again me git

--- connect.js
+++ connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 let database = () => {
     try {
         mongoose
-            .connect(process.env.MONGO_URL)
+            .connect("mongodb+srv://amanullah:Invalid_pass123@cluster0.jnbmy.mongodb.net/?retryWrites=true&w=majority")
             .then(() => console.log("DB Connection Successfull!"))
             .catch((err) => {
                 console.log(err);

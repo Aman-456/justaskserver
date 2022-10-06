00776a6e690b946129f9f03ccdc44a97ce4c2d1b
rapidev-1 <rapidev.softwares@gmail.com>
2022-10-07 00:48:17 +0500

    again me git

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -6,7 +6,7 @@ const authenticator = async (req, res, next) => {
     const auth = req.headers.authorization || req.headers.auth
     if (auth) {
         const token = auth.split(" ")[1]
-        const { id } = jwt.verify(token, process.env.SECRET_JWT)
+        const { id } = jwt.verify(token, `${process.env.SECRET_JWT}`)
         if (id) {
             req.user = id
         }
